1e61548e100b1269182906753d33086d309d1b67
Andy Truong <thehongtt@gmail.com>
2014-10-30 14:16:41 +0700

    Add $config['composer_manager_vendor_dir']

--- config.php
+++ config.php
@@ -4,3 +4,11 @@
  *
  * Important variables for system must be stored here, not custom module.
  */
+
+global $config;
+
+/**
+ * Config value for composer_manager module. More details at
+ *   https://github.com/omega8cc/boa/issues/385
+ */
+$config['composer_manager_vendor_dir'] = dirname(__FILE__) . '/vendor';
